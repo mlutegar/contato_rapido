b81fac9fa14eea6a84909677c93f4946a5ae9722
mlutegar <mlutegar@gmail.com>
2026-05-08 16:46:17 -0300

    auto: alterações 16:46:17

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/contato_rapido/',
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
   },
