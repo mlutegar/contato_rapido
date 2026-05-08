531da8e13a380a317871eb2024fc7b98a109e25e
mlutegar <mlutegar@gmail.com>
2026-05-08 16:53:00 -0300

    auto: alterações 16:53:00

--- backend/outreach_backend/settings.py
+++ backend/outreach_backend/settings.py
@@ -137,7 +137,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://mlutegar.github.io",
 ]
+
+# Allow all origins in development (be careful in production!)
+if DEBUG:
+    CORS_ALLOW_ALL_ORIGINS = True
 
 # REST Framework settings
 REST_FRAMEWORK = {
